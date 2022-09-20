ca761061004f0f52eef50b5bad1e2ac2b34f3a68
Vegzuz <vegzus@gmail.com>
2022-09-20 20:43:54 +0300

    Определяем функцию для создания массива из строк

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string[] StringArr(int num)
+{
+    string[] arr_string  = new string[num];
+    for (int i = 0; i < arr_string.Length; i++)
+    {
+        Console.WriteLine("Введите строку:");
+        arr_string[i] = Console.ReadLine();
+    }
+    return arr_string;
+}
+
+
